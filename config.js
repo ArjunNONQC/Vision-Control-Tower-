0d621544edea5cf44702fda9b7688ad32fb32678
ArjunNONQC <arjun.tomar@1mg.com>
2026-08-12 12:08:27 +0530

    Update WEBAPP_URL to the deployed script URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste the Apps Script Web App /exec URL here after deployment.
 // (Deploy > Manage deployments > Web app > Copy the URL ending in /exec)
-const WEBAPP_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbwCrq7qxK4bRQCeJjDEcykBSTjazsRoSHNSuypyJVnCEYnRfCaSad-XRxcWTXveacE53w/exec';
